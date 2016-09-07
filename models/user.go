b333e0eff82b525ccb143ed4daeea63e25b69186
kosachan <yuta@sugawarayuuta-no-MacBook-Pro.local>
2016-09-08 00:27:25 +0900

    secondcommit

---
+++ models/user.go
@@ -0,0 +1,52 @@
+package models
+ 
+import (
+    "github.com/go-xorm/xorm"
+    _ "github.com/go-sql-driver/mysql"
+)
+ 
+var engine *xorm.Engine
+ 
+ 
+// init
+func init() {
+    var err error
+    engine, err = xorm.NewEngine("mysql", "root:sy19930527@/golang")
+    if err != nil {
+        panic(err)
+    }
+}
+
+// User
+type User struct {
+    ID       int    `json:"id" xorm:"'id'"`
+    Username string `json:"name" xorm:"'nickname'"`
+}
+
+// NewUser
+func NewUser(id int, username string) User {
+    return User{
+        ID:       id,
+        Username: username,
+    }
+}
+ 
+// UserRepository
+type UserRepository struct {
+}
+
+// NewUserRepository
+func NewUserRepository() UserRepository {
+    return UserRepository{}
+}
+ 
+// GetByID
+func (m UserRepository) GetByID(id int) *User {
+    var user = User{ID: id}
+    has, _ := engine.Get(&user)
+    if has {
+        return &user
+    }
+ 
+    return nil
+}
